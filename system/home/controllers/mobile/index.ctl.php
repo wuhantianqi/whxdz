1e1ea9c183a9b1ab9c3e7f274a9effbaaa7b446b
Wang Qi <491955882@qq.com>
2017-05-09 16:31:51 +0800

    布局修复

--- system/home/controllers/mobile/index.ctl.php
+++ system/home/controllers/mobile/index.ctl.php
@@ -9,7 +9,18 @@
 class Ctl_Mobile_Index extends Ctl_Mobile {
     
     public function index(){
-        
+    	/*文章列表start*/
+    	$filter = $pager = array();
+    	$pager['page'] = 1;
+    	$pager['limit'] = $limit = 8;
+        $filter['audit'] = 1;
+        $filter['closed'] = 0;
+        $filter['from'] = $pager['from'] = "article";
+        $orderby = array('orderby'=>'ASC','article_id'=>'DESC');
+        $items = K::M('article/view')->items($filter, $orderby, $page, $limit, $count);
+        $this->pagedata['items'] = $items;
+        /*文单列表end*/
+        K::M('helper/seo')->init('mindex',array());
         $this->tmpl ='mobile/index.html';
     }
     
